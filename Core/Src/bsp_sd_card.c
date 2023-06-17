e0361183ef7b18bc0e244b7b57cac792f3d1a7c9
Tung Nguyen <tungnguyenngoc@itrvn.com>
2023-06-17 11:07:40 +0700

    update: add bsp_sd_card

---
+++ Core/Src/bsp_sd_card.c
@@ -0,0 +1,34 @@
+/**
+ * @file       <bsp_sd_card>.c
+ * @date       2023-06-17
+ *             
+ * @brief      <Handle file on SD card>
+ *             
+ * @note       None
+ */
+
+/* Includes ----------------------------------------------------------- */
+#include "bsp_sd_card.h"
+
+/* Private defines ---------------------------------------------------- */
+/* Private enumerate/structure ---------------------------------------- */
+/* Private macros ----------------------------------------------------- */
+/* Public variables --------------------------------------------------- */
+/* Private variables -------------------------------------------------- */
+/* Private function prototypes ---------------------------------------- */
+/* Function definitions ----------------------------------------------- */
+void bsp_sd_card_mount(void)
+{
+  Mount_SD("");
+}
+
+void bsp_sd_card_unmount(void)
+{
+  Unmount_SD("");
+}
+
+void bsp_sd_card_read_file_txt(char *name, char *buf)
+{
+  Read_File(name, buf);
+}
+/* End of file -------------------------------------------------------- */
